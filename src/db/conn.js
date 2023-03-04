99d87f34109799d0636cc0bdaf3099e193dd697a
Arif <arifusmani154@gmail.com>
2023-03-04 23:10:45 +0530

    vercel

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 // connection creation and create in a new db
-// mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://127.0.0.1:27017/portfolio", { useNewUrlParser: true })
 .then(()=>{
     console.log("connection successfully")
